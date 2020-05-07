073fab0b8537805a400ab580a45e52970be58676
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 15:07:49 -0400

    create trading-view component

---
+++ src/app/trading-view/trading-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trading-view',
+  templateUrl: './trading-view.component.html',
+  styleUrls: ['./trading-view.component.scss']
+})
+export class TradingViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
