3acdf25560f03ce416492f11a1110303f8e9ffa2
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-06 21:23:18 -0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReportComponent } from './report/report.component';
+import { MainComponent } from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'report/:id', component: ReportComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
