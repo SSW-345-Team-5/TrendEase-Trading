6be387cc31deca002b831bba415197e49ed1c036
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 15:57:47 -0400

    create single ticker component

---
+++ src/app/single-ticker/single-ticker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-ticker',
+  templateUrl: './single-ticker.component.html',
+  styleUrls: ['./single-ticker.component.scss']
+})
+export class SingleTickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
