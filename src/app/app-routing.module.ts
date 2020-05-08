eac8f88aae358eb8e84afecefe2685d1bc05736a
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 23:14:53 -0400

    add signal results to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReportComponent } from './report/report.component';
 import { MainComponent } from './main/main.component';
 import { SignalComponent } from './signal/signal.component';
+import { SignalResultsComponent } from './signal-results/signal-results.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'report/:id', component: ReportComponent },
-  { path: 'signal', component: SignalComponent }
+  { path: 'signal', component: SignalComponent },
+  { path: 'signal/:symbol', component: SignalResultsComponent }
 ];
 
 @NgModule({
