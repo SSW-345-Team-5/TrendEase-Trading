709527ebe889d60a57fdceee3ceadeb8a8783153
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 23:53:43 -0400

    add formatting for results display

--- src/app/signal-results/signal-results.component.ts
+++ src/app/signal-results/signal-results.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Prediction } from '../models/prediction.model';
+import { Observable } from 'rxjs';
+import { SignalService } from '../services/signal.service';
 
 @Component({
   selector: 'app-signal-results',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignalResultsComponent implements OnInit {
 
-  constructor() { }
+  symbol: string;
+  prediction$: Observable<Prediction>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private signal: SignalService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.symbol = params.symbol.toUpperCase();
+      this.prediction$ = this.signal.getSignal(this.symbol);
+    });
   }
 
 }
