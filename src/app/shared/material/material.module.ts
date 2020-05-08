845eb999f02d0d3fb82e214400c117e3cf5d1f27
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 22:46:30 -0400

    update material module

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
