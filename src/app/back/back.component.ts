b6473bd9cd387f94aeb4e8fd74d76c92df8c4565
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 23:52:58 -0400

    add back button

--- src/app/back/back.component.ts
+++ src/app/back/back.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-back',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  back() {
+    this._location.back();
   }
 
 }
