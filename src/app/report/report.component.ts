4405dde02cb1384b8313cd4a430bfa8b9f12a6a6
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-07 22:47:18 -0400

    add report components

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-report',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.id = params.id);
+  }
 
   ngOnInit(): void {
   }
