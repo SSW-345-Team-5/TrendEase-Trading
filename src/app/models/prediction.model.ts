01fdee4b5bf9435afa8a2a74054a9741ade9debb
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-10 20:13:00 -0400

    update prediction model

--- src/app/models/prediction.model.ts
+++ src/app/models/prediction.model.ts
@@ -1,4 +1,14 @@
 export interface Prediction {
-    signal: 'buy' | 'sell' | 'hold';
     success: boolean;
+    error?: PredictionError;
+    signal: 'BUY' | 'SELL' | 'HOLD';
+    prediction_proba: number[][];
+    prediction: number[];
+    prediction_time: number;
+    symbol: string;
+}
+
+interface PredictionError {
+    message: string;
+    type: string;
 }
