efc1c16a8999bd64d51fe3ca26b57302e8135a06
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-10 20:12:32 -0400

    update signal results

--- src/app/signal-results/signal-results.component.ts
+++ src/app/signal-results/signal-results.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Prediction } from '../models/prediction.model';
 import { Observable } from 'rxjs';
@@ -16,14 +16,49 @@ export class SignalResultsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private signal: SignalService
+    private signal: SignalService,
+    private _bottomSheet: MatBottomSheet
   ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.symbol = params.symbol.toUpperCase();
       this.prediction$ = this.signal.getSignal(this.symbol);
+      this.prediction$.subscribe(console.log)
     });
   }
 
+  getConfidence(prediction: Prediction): number {
+    let index = (prediction.signal === 'BUY') ? 1 : 0;
+    let confidence = 0;
+    for (var i = 0; i < prediction.prediction_proba.length; i++) {
+      confidence = confidence + prediction.prediction_proba[i][index];
+    }
+    confidence /= prediction.prediction_proba.length;
+    return confidence;
+  }
+
+  openSheet(data) {
+    this._bottomSheet.open(SignalResultsBottomSheetComponent, {
+      data: data
+    });
+  }
+
+}
+
+import { MatBottomSheet, MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+
+@Component({
+  selector: 'bottom-sheet',
+  templateUrl: 'bottom-sheet.html',
+})
+export class SignalResultsBottomSheetComponent {
+  constructor(
+    private _bottomSheetRef: MatBottomSheetRef<SignalResultsBottomSheetComponent>,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { }
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
 }
