6f1594c30d93775b4fbe514f0959ec6e68d8e71d
Robert Schaedler III <robertschaedler3@gmail.com>
2020-05-10 20:14:05 -0400

    update watchlist

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class WatchlistComponent implements OnInit {
 
   symbols = ['MSFT', 'TSLA', 'AAPL'];
+  recommended = ['FANG', 'GE', 'OXY'];
 
   constructor() { }
 
